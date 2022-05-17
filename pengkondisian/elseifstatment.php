a99ff9179d4012314a6babb8c22e244d372f44f0
edy93 <sisilrenery@gmail.com>
2022-05-17 16:06:34 +0700

    belajar pengkondisian

---
+++ pengkondisian/elseifstatment.php
@@ -0,0 +1,8 @@
+<?
+
+$nilai = 90;
+$absensi = 80;
+
+if($nilai >= 80 && $absensi >= 90){
+    echo "Nilai Anda A";
+}elseif($nilai >)
---
+++ pengkondisian/ifstetment.php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 90;
+$absen = 80;
+if ($nilai >= 100 && $absen >= 75){
+    echo "anda lulus" . PHP_EOL;
+}else{
+    echo "anda tidak lulus";
+}
